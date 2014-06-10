bd7e7ab9575f14ea41e008d4516debe1cda9c93e
anelson7@uw.edu <anelson7@uw.edu@b7b14b23-1bcd-f89e-481d-ba36562375c2>
2014-06-10 07:49:56 +0000

    commented approval and conference classes

--- trunk/ xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Model/Approval.java
+++ trunk/ xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Model/Approval.java
@@ -1,5 +1,11 @@
 package Model;
-
+/**
+ * This class is used by a Program Chair for a conference
+ * to decide whether a paper is approved by the conference.
+ * 
+ * @author Mitchell Alpert
+ *
+ */
 public enum Approval {
 	YES, NO, UNDECIDED
 }

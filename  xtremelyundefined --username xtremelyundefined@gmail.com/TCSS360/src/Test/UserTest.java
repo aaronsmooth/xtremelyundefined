1ad93648db9e792bcf876d15bbc0544ff6a65b4a
xtremelyundefined <xtremelyundefined@gmail.com>
2014-06-11 21:05:42 +0000

    added java doc to the User

---  xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Test/UserTest.java
+++  xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Test/UserTest.java
@@ -12,17 +12,27 @@ import Model.User;
 /**
  * This class tests method functionality of the User.java class.
  * 
- * @author TeamUndefined
+ * @author Aaron Nelson
  * @version 6/1/2014
  *
  */
 
 public class UserTest {
+	/**
+	 * Users to be used for testing.
+	 */
 	User usr, usr2, usr3, usr4, usr5;
-
+     
+	/**
+	 * Exception.
+	 */
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
+	/**
+	 * Initialize all the necessary fields to be used for testing.
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		usr = new User(55, "joe", "schmoe", "schmoe@gmail.com");
@@ -32,6 +42,9 @@ public class UserTest {
 		usr5 = new User(90, "jo", "doe", "o@d.com");
 	}
 	
+	/**
+	 * Test comparison functionality of the User object.
+	 */
 	@Test
 	public void compareToTest() {
 		assertTrue(usr.compareTo(usr2) < 0);
@@ -41,6 +54,9 @@ public class UserTest {
 		assertTrue(usr5.compareTo(usr2) > 0);
 	}
 	
+	/**
+	 * Test the equality of the User object.
+	 */
 	@Test
 	public void equalsTest() {
 		assertTrue(usr4.equals(usr4));
@@ -48,30 +64,45 @@ public class UserTest {
 		assertTrue(usr4.equals(usr5));
 	}
 	
+	/**
+	 * Test the getEmail() functionality of the User object.
+	 */
 	@Test
 	public void getEmailTest() {
 		assertSame(usr.getEmail(), "schmoe@gmail.com");
 		assertNotSame(usr.getEmail(), usr2.getEmail());
 	}
 	
+	/**
+	 * Test the getFirstName() functionality of the User object.
+	 */
 	@Test
 	public void getFirstNameTest() {
 		assertSame(usr.getFirstName(), "joe");
 		assertNotSame(usr.getFirstName(), usr2.getFirstName());
 	}
 	
+	/**
+	 * Test the getID() functionality of the User object.
+	 */
 	@Test
 	public void getIDTest() {
 		assertSame(usr.getID(), 55);
 		assertNotSame(usr.getID(), usr2.getID());
 	}
 	
+	/**
+	 * Test the getLastName() functionality of the User object.
+	 */
 	@Test
 	public void getLastNameTest() {
 		assertSame(usr4.getLastName(), usr3.getLastName());
 		assertNotSame(usr3.getLastName(), usr.getLastName());
 	}
 	
+	/**
+	 * Test the getName() functionality of the User object.
+	 */
 	@Test
 	public void getNameTest() {
 		String name = "schmoe, joe";
@@ -79,6 +110,9 @@ public class UserTest {
 		assertFalse(usr.getName() == name);
 	}
 	
+	/**
+	 * Test toString() functionality.
+	 */
 	@Test
 	public void toStringTest() {
 		assertTrue(usr.toString().equals(usr.getName()));

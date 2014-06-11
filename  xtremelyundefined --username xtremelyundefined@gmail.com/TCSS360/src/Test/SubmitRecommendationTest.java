e90b221218f2ba773b45bc5118b7b5591dde52c1
xtremelyundefined <xtremelyundefined@gmail.com>
2014-06-11 21:00:01 +0000

    added java doc

---  xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Test/SubmitRecommendationTest.java
+++  xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Test/SubmitRecommendationTest.java
@@ -10,17 +10,51 @@ import Model.Paper;
 import Model.User;
 import View.SubmitRecommendation;
 
+/**
+ * This class tests method functionality of the SubmitRecommendation.java class.
+ * 
+ * @author Aaron Nelson
+ * @version 6/1/2014
+ *
+ */
 public class SubmitRecommendationTest {
 
+	/**
+	 * Conference
+	 */
 	private Conference conf;
+	
+	/**
+	 * Paper to be used for testing.
+	 */
 	private Paper ppr;
+	
+	/**
+	 * paper #2 to be used for testing.
+	 */
 	private Paper ppr2;
+	
+	/**
+	 * User to be used for testing.
+	 */
 	private User usr;
+	
+	/**
+	 * User #2 to be used for testing.
+	 */
 	private User usr2;
 	
+	/**
+	 * Exception.
+	 */
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
+	/**
+	 * Initialized all the necesary fields to be used for testing.
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		usr = new User(55, "joe", "schmoe", "schmoe@gmail.com");
@@ -30,6 +64,9 @@ public class SubmitRecommendationTest {
 		conf = new Conference("alocation", "adate", "adeadline", null, "aname");	
 	}
 	
+	/**
+	 * Test the constructor functionality of the SubmitRecommendation.
+	 */
 	@Test
 	public void testWindow(){
 		SubmitRecommendation window = new SubmitRecommendation(ppr);

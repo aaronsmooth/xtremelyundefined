5b041c70f1709872430df6a100f3f55f6cb41968
xtremelyundefined <xtremelyundefined@gmail.com>
2014-06-11 20:55:51 +0000

    added javadco to the reviewtest

---  xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Test/ReviewTest.java
+++  xtremelyundefined --username xtremelyundefined@gmail.com/TCSS360/src/Test/ReviewTest.java
@@ -15,19 +15,60 @@ import Model.Paper;
 import Model.Review;
 import Model.User;
 
+/**
+ * This class tests method functionality of the Review.java class.
+ * 
+ * @author Aaron Nelson
+ * @version 6/1/2014
+ *
+ */
 public class ReviewTest {
 
+	/**
+	 * Users.
+	 */
 	User usr, usr2, usr3;
+	
+	/**
+	 * Papers.
+	 */
 	Paper ppr, ppr2;
+	
+	/**
+	 * Conference.
+	 */
 	Conference conf;
+	
+	/**
+	 * List of ratings.
+	 */
 	List<Integer> ratings;
+	
+	/**
+	 * List of authors.
+	 */
 	List<String> author;
+	
+	/**
+	 * Sub-Program Chair.
+	 */
 	String spc;
+	
+	/**
+	 * Reviewer.
+	 */
 	Review rev;
 	
+	/**
+	 * Exception.
+	 */
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
+	/**
+	 * Initialized all the neccesary fields to be used for testing.
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		usr = new User(55, "joe", "schmoe", "schmoe@gmail.com");
@@ -62,23 +103,35 @@ public class ReviewTest {
 		rev = new Review(ratings,spc, author);
 	}
 	
+	/**
+	 * Test the constructor of the Review object.
+	 */
 	@Test
 	public void ConstructorTest() {
 		rev = new Review(ratings,spc, author);
 	}
 	
+	/**
+	 * Test the getRating() functionality.
+	 */
 	@Test
 	public void getRatingTest() {
 		assertEquals(ratings, rev.getRating());
 		assertNotNull(rev.getRating());
 	}
 	
+	/**
+	 * Test the getSPCComment() functionality.
+	 */
 	@Test
 	public void getSPCCommentTest() {
 		assertEquals(spc, rev.getSpcComment());
 		assertNotNull(rev.getSpcComment());
 	}
 	
+	/**
+	 * Test the getAuthorComment() functionality.
+	 */
 	@Test
 	public void getAuthorCommentTest() {
 		assertEquals(author, rev.getAuthorComments());
